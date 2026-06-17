dafa2bec400a55a9342d3e1d2ee7fe24bc40e215
chusteeva1996-ops <chusteeva1996@gmail.com>
2026-06-17 19:28:29 +0300

    Add MakeUpperCase solution (8kyu)

---
+++ codewars/8kyu-make-uppercase.js
@@ -0,0 +1,13 @@
+// CodeWars: MakeUpperCase (8kyu)
+// Задача: преобразовать строку в верхний регистр
+// Ссылка: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+// Дата: 17.06.2026
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+// Проверка
+console.log(makeUpperCase("hello"));    // "HELLO"
+console.log(makeUpperCase("Hello"));    // "HELLO"
+console.log(makeUpperCase("Hello World")); // "HELLO WORLD"
